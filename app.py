00e4b7d54cdb4b05ac84bad0c6e4fe07ec16df67
pawangammaedge <pawan.dabi@gammaedge.io>
2024-12-20 13:42:54 +0530

    another class added in the file

--- app.py
+++ app.py
@@ -39,3 +39,27 @@ pr=even_gen(100)
 # print(next(pr))
 # print(next(pr))
 
+
+class Person:
+     def __init__(self, name, idnumber):
+          self.name=name
+          self.idnumber=idnumber
+
+     def display(self):
+          print(f"the names are: {self.name}\n and their id numbers: {self.idnumber}")
+          
+class Employee(Person):
+     def __init__(self, name, idnumber, salary):
+          super().__init__(name, idnumber)
+          self.salary=salary
+
+     def increment_hike(self, in_hike):
+          self.n=in_hike
+          salary_hike=self.salary*(self.n/100)
+          self.salary=self.salary+salary_hike
+          print(f"salary after hike: {self.salary}")
+
+     def employe_display(self):
+          print(f"the original salary is: {self.salary}")
+
+E=Employee("ajay",10120121,25000)
